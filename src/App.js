386e6f72f77e96bbc220784d3523bed31f138de2
Murad H Sarker <sarker24@gmail.com>
2022-09-12 01:48:28 +0200

    Add react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Footer from "./components/footer/Footer";
 import Home from "./components/home/Home";
+import Navbar from "./components/navbar/Navbar";
 
 function App() {
   return (
     <div className="">
-      {/*   <h1>Hello green university </h1> */}
-      <Home />
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+        <Footer />
+      </Router>
     </div>
   );
 }
--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -3,16 +3,13 @@ import Admission from "../admission/Admission";
 import Banner from "../banner/Banner";
 import BannerBackground from "../banner/BannerBackground";
 import Carusel from "../carusel/Carusel";
-import Footer from "../footer/Footer";
 import MessageChairman from "../message/MessageChairman";
 import MessageVC from "../message/MessageVC";
-import Navbar from "../navbar/Navbar";
 import WelcomeMessage from "../welcomeMessage/WelcomeMessage";
 
 const Home = () => {
   return (
     <div>
-      <Navbar />
       <Carusel />
       <Admission />
       <WelcomeMessage />
@@ -20,7 +17,6 @@ const Home = () => {
       <MessageChairman />
       <Banner />
       <BannerBackground />
-      <Footer />
     </div>
   );
 };
