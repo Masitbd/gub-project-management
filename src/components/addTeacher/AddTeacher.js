9c882db98288cc4adde9630a6f33bfea3140a7d5
sarker24 <sarker24@gmail.com>
2023-01-12 13:43:20 +0100

    Add the react hook form and create add teachers form

--- src/components/addTeacher/AddTeacher.js
+++ src/components/addTeacher/AddTeacher.js
@@ -1,7 +1,33 @@
 import React from "react";
+import { useForm } from "react-hook-form";
+import './AddTeacher.css'
+
 
 const AddTeacher = () => {
-  return <div>This is add teacher form</div>;
+  const { register, handleSubmit, formState: { errors } } = useForm();
+
+  const onSubmit = data => console.log(data);
+  return (
+    <form onSubmit={handleSubmit(onSubmit)}>
+    
+    <label>Teacher's Name:</label>
+    <input {...register("name", { required: true })} />
+    {errors.name && <span>Teacher's name is required</span>}
+    <label>Department Name:</label>
+    <select {...register("gender")}>
+        <option value="female">female</option>
+        <option value="male">male</option>
+        <option value="other">other</option>
+      </select>
+      <label>Email:</label>
+    <input {...register("email", { required: true , pattern: /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/i})} />
+    {errors.email && <span>Please enter right email address</span>}
+    <label>Research interest:</label>
+    <textarea {...register("research", { required: true })} />
+    {errors.name && <span>Please write your interest</span>}
+    <input type="submit" />
+  </form>
+  )
 };
 
 export default AddTeacher;
